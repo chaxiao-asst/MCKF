3093b7492427b93ade48e69e0a028419ac168ad8
chaxiao-asst <sh330521@163.com>
2026-04-23 17:32:23 +0800

    Update Icon.js

--- Icon.js
+++ Icon.js
@@ -1,6 +1,6 @@
 // 动态添加站点图标
 function addFavicon() {
-    const faviconUrl = '链接';
+    const faviconUrl = 'URL链接';
     const link = document.createElement('link');
     link.rel = 'icon';
     link.href = faviconUrl;
